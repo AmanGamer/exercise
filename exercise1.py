d36f5292200d5344bd670430c5745696b92e49aa
Neev05 <71703596+Neev05@users.noreply.github.com>
2020-10-01 10:59:39 +0530

    Improved it
    I have improved it if someone types letter in capital then also it can find word

--- exercise1.py
+++ exercise1.py
@@ -2,6 +2,7 @@
 
 word = input("Tell the name from dict:")
 dict1 = {"aman":"ak3258633@gmail.com", "Rohan":"rohan23589@gmail.com", "Diya":"Diya467658@gmail.com"}
-
-a = dict1.get(word)
+a = b
+b.lower()
+b = dict1.get(word)
 print("Your  serch word", word," ", a)
